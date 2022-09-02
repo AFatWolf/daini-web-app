ec751048f45d730a94177e4137c90e3e38362789
Thanh Dang Duc <thanh.dang@tokyotechies.com>
2022-09-02 23:24:55 +0700

    configure scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,17 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@formkit/nuxt', '@vueuse/nuxt', 'nuxt-lodash'],
+  css: [
+    '@/assets/styles/custom.scss',
+    '@/node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ],
+  script: [
+    '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  ],
+  // plugins: [
+  //   {
+  //     src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  //     mode: 'client',
+  //   },
+  // ],
 })

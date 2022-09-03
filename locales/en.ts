c682dd5365656fa1e698ab8edd02cbe7b10f496a
Thanh Dang Duc <thanh.dang@tokyotechies.com>
2022-09-03 09:12:23 +0700

    login, signup form ui

---
+++ locales/en.ts
@@ -0,0 +1,12 @@
+export default {
+    app_name: 'DAINI',
+    tag_line_1: 'Buy safe. Sell safe. ',
+    tag_line_2: 'No intermediary. That\'s DAINI',
+    login: 'Log In',
+    signup: 'Sign Up',
+    common: {
+        name: 'Name',
+        password: 'Password',
+        password_confirm: 'Re-Enter Password',
+    }
+}
---
+++ pages/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <tag-image />
+    <h1 class="text-primary">{{ t('hello', { name: 'vue-i18n' }) }}</h1>
+    <FormKit
+      type="text"
+      label="Your username"
+      value="calypso"
+      help="Pick a username people will remember!"
+    />
+    <form>
+      <label for="locale-select">{{ $t('language') }}: </label>
+      <select id="locale-select" v-model="$i18n.locale">
+        <option value="en">en</option>
+        <option value="fr">fr</option>
+        <option value="ja">ja</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    const { t } = useLang()
+
+    return { t }
+  },
+}
+</script>
